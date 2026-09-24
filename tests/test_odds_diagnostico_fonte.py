d655f42cca33f7e8ca633d9d084f4252248aea3e
Xaviponline <129806954+Xaviponline@users.noreply.github.com>
2026-09-24 16:29:34 +0100

    Test quota diagnosis in fixture discovery

--- tests/test_odds_diagnostico_fonte.py
+++ tests/test_odds_diagnostico_fonte.py
@@ -14,6 +14,16 @@ class Session429:
         raise requests.HTTPError("rate limit", response=response)
 
 
+class SessionQuotaFixtures:
+    def get(self, url, params=None, **kwargs):
+        response = requests.Response()
+        response.status_code = 429
+        response.url = url
+        response._content = b'{"code":"REQUEST_LIMIT_EXCEEDED"}'
+        response.headers["Content-Type"] = "application/json"
+        return response
+
+
 class FonteComFalha:
     configurada = True
     nome_fonte = "Fonte teste"
@@ -39,6 +49,17 @@ class FonteDescobertaFalha:
 
 
 class OddsDiagnosticoFonteTests(unittest.TestCase):
+
+    def test_eventos_hoje_converte_quota_em_diagnostico(self):
+        fonte = OddsBetano(
+            papi_key="teste",
+            provider="oddspapi",
+            session=SessionQuotaFixtures(),
+        )
+        fonte.PAPI_FIXTURES_INTERVALO = 0
+        self.assertEqual(fonte.eventos_hoje(), [])
+        self.assertEqual(fonte.ultimo_diagnostico_eventos, "odds_quota_esgotada")
+
     def test_odds_betano_guarda_http_sem_expor_payload(self):
         fonte = OddsBetano(
             papi_key="teste",
